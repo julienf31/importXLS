d979ee56f08d640bac4ec577781451df1fc3d806
julienf31 <julien.fournier@ynov.com>
2018-02-18 21:12:22 +0100

    New lib

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -25,8 +25,25 @@ class HomeController extends BaseController {
         Config::set('excel::import.startRow', 5);
 
         $excel = App::make('Excel');
-        $dump = $excel->load('15_UG_09Feb2018111910002.xls')->ignoreEmpty()->all()->toArray();
+        //$excel = App::make('Excel');
+        $excel->load('15_UG_09Feb2018111910002.xls', function($reader){
+            // Loop through all rows
+            $sheet = $reader->getSheet(0);
+            echo "sheet : ";
+            print_r($sheet);
+            die();
+            //$highestRow = $sheet->getHighestRow();
+            //echo $reader->getHighestRow();
+            $reader->each(function($row) {
+                //echo "<br>$row<br>";
+                if(str_contains($row,"OFF")){
+                    echo "stop";
+                }
+            });
+        });
 
+
+        die();
         $collection = array();
         foreach ($dump as $line){
             array_push($collection, $line);
@@ -37,9 +54,16 @@ class HomeController extends BaseController {
         return Redirect::route('home');
     }
 
+    public function truncateFile(){
+        Excel::load('15_UG_09Feb2018111910002.xls', function($reader) {
+
+        })->get();
+    }
+
     public function clean(){
         Data::truncate();
         return Redirect::route('home');
     }
+
 
 }
